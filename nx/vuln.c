7b959e9cb42961608cfb054592de3ce16637f96e
ahermani <agnieszka.hermaniuk.stud@pw.edu.pl>
2021-04-26 13:03:04 +0200

    correct app1

--- nx/vuln.c
+++ nx/vuln.c
@@ -1,19 +1,18 @@
-// gcc vuln.c -o out -fno-stack-protector -z execstack
+// gcc vuln.c -o out -fno-stack-protector -z execstack -m32
 #include <stdio.h>
 #include <string.h>
 
 void vuln_func()
 {
-        char input[80];
-        gets(input);
+	char input[80];
+	gets(input);
 
-        printf("It is an honour to meet you, %s\n", input);
+	printf("It is an honour to meet you, %s\n", input);
 }
 int main()
 {
-	int v;
-	printf("Stack: %p\n", &v);
 	puts("Hello adventurer, state your name");
 	vuln_func();
-        return 0;
+	return 0;
 }
+
