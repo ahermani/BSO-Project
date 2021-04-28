b54fe81af6110cac07f503201adda8d3d43a293b
ahermani <agnieszka.hermaniuk.stud@pw.edu.pl>
2021-04-28 12:17:19 +0200

    canary

--- canary/vuln.c
+++ canary/vuln.c
@@ -1,24 +1,24 @@
-#include <signal.h>
 #include <stdio.h>
 #include <string.h>
 
 void vuln_func()
 {
-	char password[20]="admin123";
-        char input[30];
-	
-        gets(input);
+	char password[20] = "admin123";
+	char input[30];
 
-        if (!strncmp(input, password, sizeof(password))){
-                printf("correct!\n");
-        }else{
-                printf("wrong!\n");
-        }
-        printf("Your input: %s\n", input);
-        printf("Actual password: %s\n", password);
+	gets(input);
+
+	if (!strncmp(input, password, sizeof(password))) {
+		printf("correct!\n");
+	} else {
+		printf("wrong!\n");
+	}
+	printf("Your input: %s\n", input);
+	printf("Actual password: %s\n", password);
 }
 int main()
 {
 	vuln_func();
-        return 0;
+	return 0;
 }
+
