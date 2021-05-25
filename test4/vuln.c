1cd832636712b85f55c90f94bec0d57169045bba
ahermani <agnieszka.hermaniuk.stud@pw.edu.pl>
2021-05-25 23:18:52 +0200

    makefile

--- test4/vuln.c
+++ test4/vuln.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  char buf[32];
-  printf("Hello! Try and ROP me:\n");
-  gets(buf);
+int main()
+{
+	char buf[32];
+	printf("Hello! Try and ROP me:\n");
+	gets(buf);
 }
